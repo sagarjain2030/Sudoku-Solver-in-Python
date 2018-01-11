42277590a560bf6b266325dc78ccc9dfd8e76315
sagarjain2030 <sagarjain2030@gmail.com>
2018-01-11 21:08:39 +0530

    Feat: Reducing Puzzle
    Combining Elimination and Only Choice strategy to solve a Sudoku Puzzle.
    This can either lead to Solved puzzle or requirement of Search strategy
    to solve sudoku.

--- solution.py
+++ solution.py
@@ -119,8 +119,25 @@ def reduce_puzzle(values):
         The values dictionary after continued application of the constraint strategies
         no longer produces any changes, or False if the puzzle is unsolvable 
     """
-    # TODO: Copy your code from the classroom and modify it to complete this function
-    raise NotImplementedError
+    stalled = False
+    while not stalled:
+        # Check how many boxes have a determined value
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+
+        # Your code here: Use the Eliminate Strategy
+        values = eliminate(values)
+
+        # Your code here: Use the Only Choice Strategy
+        values = only_choice(values)
+
+        # Check how many boxes have a determined value, to compare
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        # If no new values were added, stop the loop.
+        stalled = solved_values_before == solved_values_after
+        # Sanity check, return False if there is a box with zero available values:
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
 
 
 def search(values):
