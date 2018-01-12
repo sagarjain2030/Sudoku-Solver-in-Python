0ceaafdbfc5b068b4b0c761c959e37b24d3ad22f
sagarjain2030 <sagarjain2030@gmail.com>
2018-01-12 22:12:55 +0530

    refactor: Final solution
    Final Solution for this project.

--- solution.py
+++ solution.py
@@ -146,6 +146,7 @@ def reduce_puzzle(values):
 
         # Your code here: Use the Only Choice Strategy
         values = only_choice(values)
+        values = naked_twins(values)
 
         # Check how many boxes have a determined value, to compare
         solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
@@ -189,6 +190,8 @@ def search(values):
         attempt = search(new_sudoku)
         if attempt:
             return attempt
+
+    return values
 
 def solve(grid):
     """Find the solution to a Sudoku puzzle using search and constraint propagation
